e2a65377efe7b333ac725e5fea6a0f3ecaffefeb
Julio Rendon <juliorendon@gmail.com>
2015-11-11 10:04:31 +0100

    Splash Screen added

---
+++ app/src/main/java/jotace/org/speedtouch/SplashActivity.java
@@ -0,0 +1,37 @@
+package jotace.org.speedtouch;
+
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+
+        Thread timerThread = new Thread(){
+            public void run(){
+                try{
+                    sleep(3000);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally{
+                    Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timerThread.start();
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
+    }
+
+} //END
