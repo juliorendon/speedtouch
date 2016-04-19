1f5566b7baea54889a569e9846cfbd066bb39589
Julio Rendon <juliorendon@gmail.com>
2016-04-19 16:49:06 +0200

    a

--- app/src/main/java/jotace/org/speedtouch/AddActivity.java
+++ app/src/main/java/jotace/org/speedtouch/AddActivity.java
@@ -1,11 +1,15 @@
 package jotace.org.speedtouch;
 
 
+import android.content.ContentUris;
 import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Typeface;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -18,10 +22,17 @@ import android.widget.Toast;
 import com.soundcloud.android.crop.Crop;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class AddActivity extends AppCompatActivity {
 
     private ImageView contactImage;
+    private EditText nameEditText;
+    private EditText numberEditText;
+    private static final int REQUEST_CODE_PICK_CONTACTS = 7;
+    private Uri uriContact;
+    private String contactID;     // contacts unique ID
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +45,17 @@ public class AddActivity extends AppCompatActivity {
         // Setting custom font to main title
         TextView txtTitle = (TextView)findViewById(R.id.main_title);
         txtTitle.setTypeface(font);
+
+        // Getting Pick from Contacts Button
+        Button pickFromContactsBtn = (Button) findViewById(R.id.btn_pick_from_contacts);
+        pickFromContactsBtn.setTypeface(font);
+
+        pickFromContactsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pickContactFromContacts();
+            }
+        });
 
         // Gettting ImageView
         contactImage = (ImageView) findViewById(R.id.contact_image);
@@ -58,8 +80,8 @@ public class AddActivity extends AppCompatActivity {
         });
 
         // Getting Edit Texts
-        final EditText nameEditText = (EditText) findViewById(R.id.contact_name);
-        final EditText numberEditText = (EditText) findViewById(R.id.contact_phone);
+        nameEditText = (EditText) findViewById(R.id.contact_name);
+        numberEditText = (EditText) findViewById(R.id.contact_phone);
 
         //Getting Save Button
         Button saveBtn = (Button) findViewById(R.id.btn_save);
@@ -113,6 +135,11 @@ public class AddActivity extends AppCompatActivity {
             beginCrop(result.getData());
         } else if (requestCode == Crop.REQUEST_CROP) {
             handleCrop(resultCode, result);
+        } else if (requestCode == REQUEST_CODE_PICK_CONTACTS && resultCode == RESULT_OK) {
+            uriContact = result.getData();
+            retrieveContactNumber();
+            retrieveContactName();
+            retrieveContactPhoto();
         }
     }
 
@@ -129,4 +156,85 @@ public class AddActivity extends AppCompatActivity {
         }
     }
 
-}
+    private void pickContactFromContacts(){
+        Intent pickFromContactsIntent = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);
+        startActivityForResult(pickFromContactsIntent, REQUEST_CODE_PICK_CONTACTS);
+    }
+
+
+    private void retrieveContactName() {
+        String contactName = null;
+
+        // Querying contact data store
+        Cursor cursor = getContentResolver().query(uriContact, null, null, null, null);
+
+        if (cursor.moveToFirst()) {
+            // DISPLAY_NAME = The display name for the contact.
+            contactName = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+        }
+
+        // Setting the Name on the Edit Text
+        nameEditText.setText(contactName);
+
+        cursor.close();
+    }
+
+    private void retrieveContactNumber() {
+
+        String contactNumber = null;
+
+        // getting contacts ID
+        Cursor cursorID = getContentResolver().query(uriContact, new String[]{ContactsContract.Contacts._ID}, null, null, null);
+
+        if (cursorID.moveToFirst()) {
+
+            contactID = cursorID.getString(cursorID.getColumnIndex(ContactsContract.Contacts._ID));
+        }
+
+        cursorID.close();
+
+        // Using the contact ID now we will get contact phone number
+        Cursor cursorPhone = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                new String[]{ContactsContract.CommonDataKinds.Phone.NUMBER},
+
+                ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ? AND " +
+                        ContactsContract.CommonDataKinds.Phone.TYPE + " = " +
+                        ContactsContract.CommonDataKinds.Phone.TYPE_MOBILE,
+
+                new String[]{contactID},
+                null);
+
+        if (cursorPhone.moveToFirst()) {
+            contactNumber = cursorPhone.getString(cursorPhone.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+        }
+
+        // Setting the Phone Number on the Edit Text
+        numberEditText.setText(contactNumber);
+
+        cursorPhone.close();
+    }
+
+    private void retrieveContactPhoto() {
+
+        Bitmap photo = null;
+
+        try {
+            InputStream inputStream = ContactsContract.Contacts.openContactPhotoInputStream(getContentResolver(),
+                    ContentUris.withAppendedId(ContactsContract.Contacts.CONTENT_URI, new Long(contactID)));
+
+            if (inputStream != null) {
+                photo = BitmapFactory.decodeStream(inputStream);
+                 contactImage.setImageBitmap(photo);
+            }
+
+            assert inputStream != null;
+            inputStream.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+} // END
